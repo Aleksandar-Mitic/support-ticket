e104c363c4629f74709e7d888db7f4247cab40fa
Aleksandar Mitic <aleksandar.webart@gmail.com>
2019-06-13 14:36:14 +0200

    Added Ticket model data to migration tabče

--- database/migrations/2019_06_13_123409_create_tickets_table.php
+++ database/migrations/2019_06_13_123409_create_tickets_table.php
@@ -14,7 +14,14 @@ class CreateTicketsTable extends Migration
     public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->string('ticket_id')->unique();
+            $table->string('title');
+            $table->string('priority');
+            $table->text('message');
+            $table->string('status');
             $table->timestamps();
         });
     }
