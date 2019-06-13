602f583a2b4516e13f99b262b41fcdde997ed325
Aleksandar Mitic <aleksandar.webart@gmail.com>
2019-06-13 14:34:42 +0200

    Added IDE helper & Ticker model -a added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // PHPStorm  helper package registration
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
